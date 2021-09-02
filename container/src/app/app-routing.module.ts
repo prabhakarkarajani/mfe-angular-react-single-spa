68c06e736e31999c3448a3e24e4552000e2cf689
prabhakar.webdeveloper@gmail.com <sam@0521>
2021-09-02 15:25:06 +0530

    add filed

---
+++ container/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [{
+  path: 'child1',
+  children: [{
+    path: '**',
+    loadChildren: () => import('./spa-host/spa-host.module').then(m => m.SpaHostModule),
+    data: { app: 'child1' }
+  }]
+}, {
+  path: 'child2',
+  children: [{
+    path: '**',
+    loadChildren: () => import('./spa-host/spa-host.module').then(m => m.SpaHostModule),
+    data: { app: 'child2' }
+  }]
+},
+{
+  path: 'child3',
+  children: [{
+    path: '**',
+    loadChildren: () => import('./spa-host/spa-host.module').then(m => m.SpaHostModule),
+    data: { app: 'child3' }
+  }]
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ container/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SpaHostComponent } from './spa-host/spa-host.component';
+import { RouteReuseStrategy } from '@angular/router';
+import { MicroFrontendRouteReuseStrategy } from 'src/services/route-reuse-strategy';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [{
+    provide: RouteReuseStrategy,
+    useClass: MicroFrontendRouteReuseStrategy
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
